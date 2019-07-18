7f61b54b6b681c944774ff168bce94a22317fd6b
levi97 <30956324+levi97@users.noreply.github.com>
2019-07-18 12:39:57 -0700

    Update policy.py

--- rl/policy.py
+++ rl/policy.py
@@ -149,6 +149,7 @@ class EpsGreedyQPolicy(Policy):
     def __init__(self, eps=.1):
         super(EpsGreedyQPolicy, self).__init__()
         self.eps = eps
+        self.count = 0
 
     def select_action(self, q_values):
         """Return the selected action
@@ -166,8 +167,13 @@ class EpsGreedyQPolicy(Policy):
             action = np.random.randint(0, nb_actions)
         else:
             action = np.argmax(q_values)
+        
         if self.eps > 0.15:
             self.eps -= int(self.agent.step/10000)*0.1
+            self.count += 1
+            if self.count > 10000:
+                print(self.eps)
+                self.count = 0
         return action
             
     def get_config(self):
