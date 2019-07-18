18b9d3ba2a023f377e16e342796d81df122b8bf6
levi97 <30956324+levi97@users.noreply.github.com>
2019-07-18 01:24:31 -0700

    Update policy.py

--- rl/policy.py
+++ rl/policy.py
@@ -168,7 +168,7 @@ class EpsGreedyQPolicy(Policy):
             action = np.argmax(q_values)
         
         if self.eps > 0.06:
-            self.eps -= int(self.agent.steps/100)*0.05
+            self.eps -= int(self.agent.step/100)*0.05
             print(self.eps)
             
         return action
